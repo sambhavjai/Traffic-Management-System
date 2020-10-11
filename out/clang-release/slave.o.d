de63d8e5131d00ea92be2efd485da9890a380aba
sambhavjai <38015768+sambhavjai@users.noreply.github.com>
2020-10-11 13:58:43 +0530

    added message handling

--- out/clang-release/slave.o.d
+++ out/clang-release/slave.o.d
@@ -1,3 +1,6 @@
-out/clang-release//slave.o: slave.cc C:/omnetpp-5.6.2/include\omnetpp.h
+out/clang-release//slave.o: slave.cc C:/omnetpp-5.6.2/include\omnetpp.h \
+  outgoing_m.h
 
 C:/omnetpp-5.6.2/include\omnetpp.h:
+
+outgoing_m.h:
--- slave.cc
+++ slave.cc
@@ -1,11 +1,17 @@
 #include <omnetpp.h>
+#include <outgoing_m.h>
 
 using namespace omnetpp;
 
+class outgoing_message:public Outgoing_Base
+{
+    virtual outgoing_message *dup() const override {return new outgoing_message(*this);}
+};
 class slave: public cSimpleModule
 {
     virtual void initialize() override;
     virtual void handleMessage(cMessage *msg) override;
+    //void handleMessage(outgoing_message *msg);
 };
 Define_Module(slave);
 
@@ -13,8 +19,8 @@ void slave::initialize()
 {
 
 }
-
 void slave::handleMessage(cMessage *msg)
 {
-    if(msg);
+    outgoing_message *ttmsg = check_and_cast<outgoing_message *>(msg);
+    EV<<"Message recieved with node id "<<ttmsg->getNode_id()<<"and green light time"<<ttmsg->getGreen_light_time();
 }
